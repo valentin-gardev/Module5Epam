c44c4caf5da687d56ffa804a8e2cf6ba242e2653
Valentin Gardev <bestmaad@gmail.com>
2026-07-02 01:47:10 +0300

    Task 3 starting

---
+++ Task3/tests/specs/testCases.spec.js
@@ -0,0 +1,36 @@
+
+
+test('User logs in successfully', async({ page }) => {
+
+
+})
+
+test('User can browse product categories', async({ page }) => {
+
+
+})
+
+test('User can open a product and add it to cart', async({ page }) => {
+
+
+})
+
+test('User can complete purchase form', async({ page }) => {
+
+
+})
+
+test('User opens Contact modal and submits a message', async({ page }) => {
+
+
+})
+
+test('User can hover over product card', async({ page }) => {
+
+
+})
+
+test('User logs in successfully', async({ page }) => {
+
+
+})
